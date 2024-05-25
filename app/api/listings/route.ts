d5c1776626783aeda235a07b159e2dcc1841db57
Sparsh Jain <jainsparsh17@gmail.com>
2024-05-26 01:47:58 +0530

    API endpoint added for listings creation

---
+++ app/api/listings/route.ts
@@ -0,0 +1,55 @@
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  const body = await request.json();
+  const {
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+  } = body;
+
+  if (
+    !title ||
+    !description ||
+    !imageSrc ||
+    !roomCount ||
+    !bathroomCount ||
+    !guestCount ||
+    !category ||
+    !location ||
+    !price
+  ) {
+    return new NextResponse("Missing required fields", { status: 400 });
+  }
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing, { status: 201 });
+}
